b77c63db00ee76d5f19b08d8f2506e26d3fc6268
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2024-12-12 18:03:33 +0100

    api key extractet

--- API/Auth.js
+++ API/Auth.js
@@ -1,6 +1,4 @@
-const apiKey =
-    'patLWKKgBwq4fHn4M.47c838648d46a5cb0d3fc8a7e2423d59214d46a4030b16b0390874bd0311a875';
-const baseId = 'appOu5rHbNZ9bIQFC';
+import { apiKey, baseId } from "./config";
 const tableName = 'Users';
 
 document.addEventListener('DOMContentLoaded', async () => {
---
+++ API/config.js
@@ -0,0 +1,3 @@
+export const apiKey =
+    'patLWKKgBwq4fHn4M.47c838648d46a5cb0d3fc8a7e2423d59214d46a4030b16b0390874bd0311a875';
+export const baseId = 'appOu5rHbNZ9bIQFC';
